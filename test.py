abdd555681184b522f4ded6b58031f946c7312fb
estods3 <you@example.com>
2020-03-07 13:12:21 -0500

    implemented for ROS. not yet working

--- test.py
+++ test.py
@@ -1,35 +1,75 @@
-import numpy as np 
+#!/usr/bin/env python2
+import sys
+import rospy
+import numpy as np
 import cv2
+
+from sensor_msgs.msg import CompressedImage
+
 
 from visual_odometry import PinholeCamera, VisualOdometry
 
+class SLAMrunner:
+    def __init__(self):
+        #fx: 848.993	 fy: 840.167 	cx: 374.031 	cy: 222.626
+        self.camModel = PinholeCamera(640, 480, 848.993, 840.167, 374.031, 222.626)
+        self.vo = VisualOdometry(self.camModel)
+        self.traj = np.zeros((600,600,3), dtype=np.uint8)
+        self.image_sub = rospy.Subscriber("/camera/image_raw", CompressedImage, self.callback, queue_size=1)
+        self.img_id = 0
 
-cam = PinholeCamera(1241.0, 376.0, 718.8560, 718.8560, 607.1928, 185.2157)
-vo = VisualOdometry(cam, '/home/xxx/datasets/KITTI_odometry_poses/00.txt')
+    def callback(self, data):
+        ## Get Image
+        try:
+            #img = self.bridge.imgmsg_to_cv2(data, "mono8")
+            np_arr = np.fromstring(data.data, np.uint8)
+            img = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
+        except CvBridgeError as e:
+            print(e)
 
-traj = np.zeros((600,600,3), dtype=np.uint8)
+        ## Upscale Image Size
+        scale_percent = 100 * 20 * 2 # percent of original size
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        print(width)
+        # resize image
+        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-for img_id in xrange(4541):
-	img = cv2.imread('/home/xxx/datasets/KITTI_odometry_gray/00/image_0/'+str(img_id).zfill(6)+'.png', 0)
+	self.vo.update(img, self.img_id)
 
-	vo.update(img, img_id)
+	cur_t = self.vo.cur_t
 
-	cur_t = vo.cur_t
 	if(img_id > 2):
 		x, y, z = cur_t[0], cur_t[1], cur_t[2]
 	else:
 		x, y, z = 0., 0., 0.
 	draw_x, draw_y = int(x)+290, int(z)+90
-	true_x, true_y = int(vo.trueX)+290, int(vo.trueZ)+90
+	true_x, true_y = int(self.vo.trueX)+290, int(self.vo.trueZ)+90
 
-	cv2.circle(traj, (draw_x,draw_y), 1, (img_id*255/4540,255-img_id*255/4540,0), 1)
-	cv2.circle(traj, (true_x,true_y), 1, (0,0,255), 2)
-	cv2.rectangle(traj, (10, 20), (600, 60), (0,0,0), -1)
+	cv2.circle(self.traj, (draw_x,draw_y), 1, (self.img_id*255/4540,255-self.img_id*255/4540,0), 1)
+	cv2.circle(self.traj, (true_x,true_y), 1, (0,0,255), 2)
+	cv2.rectangle(self.traj, (10, 20), (600, 60), (0,0,0), -1)
 	text = "Coordinates: x=%2fm y=%2fm z=%2fm"%(x,y,z)
-	cv2.putText(traj, text, (20,40), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1, 8)
+	cv2.putText(self.traj, text, (20,40), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1, 8)
 
 	cv2.imshow('Road facing camera', img)
-	cv2.imshow('Trajectory', traj)
+	cv2.imshow('Trajectory', self.traj)
 	cv2.waitKey(1)
 
-cv2.imwrite('map.png', traj)
+    def saveMap(self):
+        cv2.imwrite('map.png', self.traj)
+
+def main(args):
+    slam = SLAMrunner()
+    rospy.init_node('pc_map_maker_node', anonymous=True)
+    rospy.spin()
+    print("Map Maker: Node Shutting down")
+    slam.saveMap()
+    print("Map Maker: Map Saved to File")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
--- visual_odometry.py
+++ visual_odometry.py
@@ -6,11 +6,12 @@ STAGE_SECOND_FRAME = 1
 STAGE_DEFAULT_FRAME = 2
 kMinNumFeature = 1500
 
-lk_params = dict(winSize  = (21, 21), 
+lk_params = dict(winSize  = (10, 10), 
 				#maxLevel = 3,
              	criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
 
 def featureTracking(image_ref, image_cur, px_ref):
+        print(px_ref)
 	kp2, st, err = cv2.calcOpticalFlowPyrLK(image_ref, image_cur, px_ref, None, **lk_params)  #shape: [k,2] [k,1] [k,1]
 
 	st = st.reshape(st.shape[0])
@@ -34,7 +35,7 @@ class PinholeCamera:
 
 
 class VisualOdometry:
-	def __init__(self, cam, annotations):
+	def __init__(self, cam):
 		self.frame_stage = 0
 		self.cam = cam
 		self.new_frame = None
@@ -45,10 +46,10 @@ class VisualOdometry:
 		self.px_cur = None
 		self.focal = cam.fx
 		self.pp = (cam.cx, cam.cy)
-		self.trueX, self.trueY, self.trueZ = 0, 0, 0
-		self.detector = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
-		with open(annotations) as f:
-			self.annotations = f.readlines()
+		#self.trueX, self.trueY, self.trueZ = 0, 0, 0
+		self.detector = cv2.FastFeatureDetector_create(threshold=255, nonmaxSuppression=False)
+		#with open(annotations) as f:
+		#	self.annotations = f.readlines()
 
 	def getAbsoluteScale(self, frame_id):  #specialized for KITTI odometry dataset
 		ss = self.annotations[frame_id-1].strip().split()
@@ -78,10 +79,10 @@ class VisualOdometry:
 		self.px_ref, self.px_cur = featureTracking(self.last_frame, self.new_frame, self.px_ref)
 		E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC, prob=0.999, threshold=1.0)
 		_, R, t, mask = cv2.recoverPose(E, self.px_cur, self.px_ref, focal=self.focal, pp = self.pp)
-		absolute_scale = self.getAbsoluteScale(frame_id)
-		if(absolute_scale > 0.1):
-			self.cur_t = self.cur_t + absolute_scale*self.cur_R.dot(t) 
-			self.cur_R = R.dot(self.cur_R)
+		#absolute_scale = self.getAbsoluteScale(frame_id)
+		#if(absolute_scale > 0.1):
+		#	self.cur_t = self.cur_t + absolute_scale*self.cur_R.dot(t) 
+		#	self.cur_R = R.dot(self.cur_R)
 		if(self.px_ref.shape[0] < kMinNumFeature):
 			self.px_cur = self.detector.detect(self.new_frame)
 			self.px_cur = np.array([x.pt for x in self.px_cur], dtype=np.float32)
